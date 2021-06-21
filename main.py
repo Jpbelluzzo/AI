754676e530960066b2591dc938efc7548083acae
Jpbelluzzo <jpbelluzzo@usp.br>
2021-06-21 20:11:55 -0300

    BFS implemented

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from graphs_gen import knn_generator
-import dfs
+import searches
 import json
 import matplotlib.pyplot as plt
 
-graph, x_coords, y_coords, connections = knn_generator(4, 30, 500, 500)
+graph, x_coords, y_coords, connections = knn_generator(4, 15, 500, 500)
 
 for i in range(len(graph)):
     plt.annotate(str(i), (x_coords[i],y_coords[i]))
@@ -14,8 +14,11 @@ for connection in connections:
     point2 = connection[1]
     plt.plot((graph[point1]['x'], graph[point2]['x']),(graph[point1]['y'], graph[point2]['y']),'r')
 
-dfs_search = dfs.depth_first_search(11, 23, graph, connections)
+dfs_search = searches.depth_first_search(11, 8, graph, connections)
 print(dfs_search)
+
+bfs_search = searches.breadth_first_search(11, 8, graph, connections)
+print(bfs_search)
 
 plt.savefig('teste.png')
 #print(json.dumps(dict.__repr__(graph), indent=4))
---
+++ searches.py
@@ -0,0 +1,37 @@
+# Depth-First Search Algorithm
+
+def depth_first_search(init_node, end_node, graph, connections):
+    visited = []
+    visited = dfs_recursion(init_node, end_node, visited, graph, connections)
+    return visited
+
+def dfs_recursion(node, end_node, visited, graph, connections):
+    visited.append(node)
+    if node == end_node:
+        return visited
+    neighbours = graph[node]['connections']
+    for neighbour in neighbours:
+        if neighbour not in visited:
+            #print(node, neighbour)
+            dfs_recursion(neighbour, end_node, visited, graph, connections)
+            if(visited[-1] == end_node):
+                return visited
+
+# Breadth-First Search Algorithm
+
+def breadth_first_search(init_node, end_node, graph, connections):
+    path = []
+    queue = [init_node]
+    bfs_recursion(init_node, end_node, path, queue, graph, connections)
+    return path
+
+def bfs_recursion(node, end_node, path, queue, graph, connections):
+    path.append(node)
+    if node == end_node:
+        return path
+    neighbours = graph[node]['connections']
+    for neighbour in neighbours:
+        if neighbour not in queue:        
+            queue += [neighbour]
+    del(queue[0])
+    bfs_recursion(queue[0], end_node, path, queue, graph, connections)
